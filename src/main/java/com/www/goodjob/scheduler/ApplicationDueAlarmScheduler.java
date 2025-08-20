104164ecddc50fdd3e17b2583314b01a17665591
21YuJin <you5463@ajou.ac.kr>
2025-08-20 23:35:34 +0900

    TEST : 23:35 -> 50

--- src/main/java/com/www/goodjob/scheduler/ApplicationDueAlarmScheduler.java
+++ src/main/java/com/www/goodjob/scheduler/ApplicationDueAlarmScheduler.java
@@ -33,7 +33,7 @@ public class ApplicationDueAlarmScheduler {
 
     /** 매일 10:00 KST */
     // @Scheduled(cron = "0 0 10 * * *", zone = "Asia/Seoul")
-    @Scheduled(cron = "0 35 23 * * *", zone = "Asia/Seoul")
+    @Scheduled(cron = "0 50 23 * * *", zone = "Asia/Seoul")
     public void run() {
         LocalDate today = LocalDate.now(ZONE);
         LocalDate start = today.plusDays(DUE_START_OFFSET);
